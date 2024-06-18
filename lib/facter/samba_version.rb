3ac741ef33503bc9b88262f606b62009c5009eb9
Maximilian Gaß <m.gass@babiel.com>
2024-06-18 15:44:20 +0200

    fix: completely rewrite samba_version fact
    This brings the fact implementation in line with how custom facts
    should be. It also solves several problems.
    
    - simpler and clearer
    - the fact code is no longer executed by just loading the file
    - remove call to FileTest.exists? which was removed in Ruby 3.2

--- lib/facter/samba_version.rb
+++ lib/facter/samba_version.rb
@@ -1,32 +1,14 @@
-#!/usr/bin/ruby
-require 'facter'
-
-package = 'samba'
-
-
-distid = Facter.value(:operatingsystem)
-
-case distid
-    when /RedHatEnterprise|CentOS|Fedora|RHEL/
-        if  FileTest.exists?("/usr/bin/yum")
-            version = Facter::Util::Resolution.exec('/usr/bin/yum info samba | sed \'s/Version *: \([0-9\.]\+\)/\1/gp;d\' | head -n 1')
-        end
-    when /Ubuntu|Debian/
-        if  FileTest.exists?("/usr/bin/apt-cache")
-            version = Facter::Util::Resolution.exec('apt-cache show samba | sed \'s/Version:.*:\([0-9\.]\+\).*/\1/gp;d\' | head -n 1')
-        end
-    when 'Archlinux'
-      version = Facter::Util::Resolution.exec('pacman -Si samba | awk "/Version/ {print $3}" | cut -d"-" -f1')
-    else
-        version = "0.0.0"
+Facter.add(:samba_version) do
+  confine package_provider: 'yum'
+  setcode '/usr/bin/yum info samba | sed \'s/Version *: \([0-9\.]\+\)/\1/gp;d\' | head -n 1'
 end
 
-#print distid
-#print version
-
-Facter.add("samba_version") do
-    setcode do
-        version
-    end
+Facter.add(:samba_version) do
+  confine package_provider: 'apt'
+  setcode 'apt-cache show samba | sed \'s/Version:.*:\([0-9\.]\+\).*/\1/gp;d\' | head -n 1'
 end
 
+Facter.add(:samba_version) do
+  confine package_provider: 'pacman'
+  setcode 'pacman -Si samba | awk "/Version/ {print $3}" | cut -d"-" -f1'
+end
